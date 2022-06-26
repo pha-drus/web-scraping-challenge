b1d69a528b4125431484195e01c7539b5a547f15
pha-drus <the.rick.springer@gmail.com>
2022-06-25 21:50:45 -0500

    /scrape fix

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+import scrape_mars
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def index():
 @app.route("/scrape")
 def scrape():
     mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
+    mars_data = scrape_mars.scrape_all()
     mars.update_one({}, {"$set":mars_data}, upsert=True)
     return redirect('/', code=302)
 
